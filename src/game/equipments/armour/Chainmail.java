e3592691e52124dcb846135fe58aefd13e1ed8f7
yumyum-web <yumethsumathipala@gmail.com>
2024-03-12 15:59:58 +0530

    chore: refactor Equipment subclasses
    Signed-off-by: yumyum-web <yumethsumathipala@gmail.com>

---
+++ src/game/equipments/armour/Chainmail.java
@@ -0,0 +1,10 @@
+package game.equipments.armour;
+
+import game.equipments.Equipment;
+import game.equipments.EquipmentType;
+
+public class Chainmail extends Equipment {
+    public Chainmail() {
+        super("Chainmail", EquipmentType.Armour, 70, 0, 1, 0, -1);
+    }
+}
---
+++ src/game/equipments/armour/Regalia.java
@@ -0,0 +1,10 @@
+package game.equipments.armour;
+
+import game.equipments.Equipment;
+import game.equipments.EquipmentType;
+
+public class Regalia extends Equipment {
+    public Regalia() {
+        super("Regalia", EquipmentType.Armour, 105, 0, 1, 0, 0);
+    }
+}
